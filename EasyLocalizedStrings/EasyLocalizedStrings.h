1af9ddc4ef8fd1675d90783b85c5e3b551a72490
Alex <tucanae@icloud.com>
2019-01-23 11:02:49 +0000

    Initial Commit

---
+++ EasyLocalizedStrings/EasyLocalizedStrings.h
@@ -0,0 +1,19 @@
+//
+//  EasyLocalizedStrings.h
+//  EasyLocalizedStrings
+//
+//  Created by Alex on 23/01/2019.
+//  Copyright © 2019 Tucanae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyLocalizedStrings.
+FOUNDATION_EXPORT double EasyLocalizedStringsVersionNumber;
+
+//! Project version string for EasyLocalizedStrings.
+FOUNDATION_EXPORT const unsigned char EasyLocalizedStringsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyLocalizedStrings/PublicHeader.h>
+
+
